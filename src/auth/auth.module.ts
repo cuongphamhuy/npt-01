ebde67bcb2a229330e026fe4aff6531d2179c6b0
cuongphamhuy <kts.mungtoi@gmail.com>
2020-12-07 20:51:20 +0700

    refactor

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
 import { UsersModule } from '../users/users.module';
+import { AuthController } from './auth.controller';
 import { PassportModule } from '@nestjs/passport';
 import { SessionSerializer } from './session.serializer';
 
@@ -9,5 +10,6 @@ import { SessionSerializer } from './session.serializer';
   imports: [PassportModule, UsersModule],
   providers: [AuthService, LocalStrategy, SessionSerializer],
   exports: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
